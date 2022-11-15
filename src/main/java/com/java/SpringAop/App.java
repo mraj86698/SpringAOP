f79aab6cbcd57cce35ca1e04ab4b378305e10dcf
MohanRaj <mraj86698@gmail.com>
2022-11-15 11:23:25 +0530

    Added to Welcome message

---
+++ src/main/java/com/java/SpringAop/App.java
@@ -0,0 +1,13 @@
+package com.java.SpringAop;
+
+
+public class App {
+	public static void main(String[] args) {
+		/**
+		 * Welcome Message to Display
+		 *
+		 */
+		System.out.println("Welcome to the SpringAop");
+
+	}
+}
